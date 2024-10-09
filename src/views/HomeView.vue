7481582fa30586d356899da9be349a0f32ffdf35
prasert <prasert.pom@bbl.co.th>
2024-10-09 11:11:20 +0700

    innit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="home">
+    <button @click="dereaseCounter" class="btn">-</button>
+    <span class="counter">{{ counter }}</span>
+    <button @click="increaseCounter" class="btn">+</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const counter = ref(0);
+
+const increaseCounter = () => {
+  counter.value++;
+};
+
+const dereaseCounter = () => {
+  counter.value--;
+};
+</script>
+
+<!-- <script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const counter = ref(0)
+
+    const increaseCounter = () => {
+      counter.value++
+
+    }
+
+    const dereaseCounter = () => {
+      counter.value--
+
+    }
+
+    return {
+      counter, increaseCounter, dereaseCounter
+
+    }
+
+  }
+
+}  
+
+</script>
+  <script>
+export default
+  {
+    data() {
+      return {
+        counter: 0
+
+      }
+
+
+
+    }, methods: {
+
+      increaseCounter() {
+        this.counter++
+
+      },
+      dereaseCounter() {
+        this.counter--
+
+      }
+    }
+
+  }
+</script> -->
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
